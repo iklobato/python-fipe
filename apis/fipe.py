f890f838c046de000dfa3f47190de1503510573f
henriquelobato <iklobato1@gmail.com>
2023-08-09 23:16:06 -0300

    fix(base_api): build_url

--- apis/fipe.py
+++ apis/fipe.py
@@ -1,5 +1,3 @@
-from abc import ABC
-
 from apis.base import BaseApi
 
 
@@ -7,18 +5,5 @@ class FipeApi(BaseApi):
 
     sub_path = ""
 
-    async def get(self, endpoint, **kwargs):
-        async with self.session.get(url=f"{self.base_url}{endpoint}", **kwargs) as response:
-            return response
-
-    async def post(self, endpoint, **kwargs):
-        async with self.session.post(url=f"{self.base_url}{endpoint}", **kwargs) as response:
-            return response
-
-    async def put(self, endpoint, **kwargs):
-        async with self.session.put(url=f"{self.base_url}{endpoint}", **kwargs) as response:
-            return response
-
-    async def delete(self, endpoint, **kwargs):
-        async with self.session.delete(url=f"{self.base_url}{endpoint}", **kwargs) as response:
-            return response
+    def build_url(self, subpath: str, **kwargs):
+        return f"{self.base_url}{subpath}".format(**kwargs)
